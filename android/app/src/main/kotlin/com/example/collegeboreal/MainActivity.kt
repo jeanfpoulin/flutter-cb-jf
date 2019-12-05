edadf3d0c1704196b0e916502dc5748a2b12765e
jeanfpoulin <jeanfpoulin@gmail.com>
2019-12-05 15:30:45 -0500

    Kotlin update

--- android/app/src/main/kotlin/com/example/collegeboreal/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/collegeboreal/MainActivity.kt
@@ -1,10 +1,13 @@
 package ca.collegeboreal.appmobile
 
-import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
 }
