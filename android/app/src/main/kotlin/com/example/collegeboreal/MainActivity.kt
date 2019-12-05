a15edd133231242a01542aa0ad42d052c07b55ef
jeanfpoulin <jeanfpoulin@gmail.com>
2019-12-05 15:20:16 -0500

    revert

--- android/app/src/main/kotlin/com/example/collegeboreal/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/collegeboreal/MainActivity.kt
@@ -5,11 +5,6 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-import io.flutter.app.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-    }
+override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
 }
